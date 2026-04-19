c3d785849e1bc5178ffa103ec4711fafb4cffe0f
Ishanthingorani <161329259+Ishanthingorani@users.noreply.github.com>
2026-04-19 21:41:20 +0530

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import re
 from rapidfuzz import process, fuzz
 
-st.title("KESTONE")
 st.markdown("<h1 style='color:red;'>KESTONE</h1>", unsafe_allow_html=True)
 st.title("Company Fuzzy Matching Tool")
 
